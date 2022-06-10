ba19c74e6b674a00a11063fcb7e8f36b21b971d8
Nelcione Dias <nelcionedias@gmail.com>
2022-06-10 11:26:55 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TESTE 1: seller findById ===");
@@ -29,6 +33,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 4: seller Insert ===");
+		Seller newSeller =  new Seller(null, "Greg", "greg@gmail.com", new Date(),4000.00, department);// cria o objeto
+		sellerDao.insert(newSeller);//inserindo no banco de dados
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 		
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,53 +16,84 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
-	
-	//cria dependencia de uma coneção
-	public SellerDaoJDBC (Connection conn) {
+
+	// cria dependencia de uma coneção
+	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
+
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+						"INSERT INTO seller " 
+						+ "( Name, Email, BirthDate, BaseSalary, DepartmentId) "
+						+ "VALUES " 
+						+ "(?, ?, ?, ?, ?)", 
+						Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getEmail());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if( rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
 		/*
-		 * SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id Where seller.Id = ?
-		 * */
+		 * SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department
+		 * ON seller.DepartmentId = department.Id Where seller.Id = ?
+		 */
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement(
-						"SELECT seller.*,department.Name as DepName "
-						+ "FROM seller INNER JOIN department "
-						+ "ON seller.DepartmentId = department.Id " 
-						+ "Where seller.Id = ?");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "Where seller.Id = ?");
+
 			st.setInt(1, id);// faz a consulta passando o id
 			rs = st.executeQuery(); // guarda o resultado dentro de um result set
-			
+
 			// trasformando result set em objeto
-			//verifica se veio um resultado transforma em um objeto
+			// verifica se veio um resultado transforma em um objeto
 			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
 				Seller obj = instantiateSeller(rs, dep);
@@ -88,7 +120,7 @@ public class SellerDaoJDBC implements SellerDao{
 		return obj;
 	}
 
-	private Department instantiateDepartment(ResultSet rs) throws SQLException {//propaga o SQLException
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {// propaga o SQLException
 		Department dep = new Department();
 		dep.setId(rs.getInt("DepartmentId"));
 		dep.setName(rs.getString("DepName"));
@@ -100,27 +132,26 @@ public class SellerDaoJDBC implements SellerDao{
 
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name");
+
 			rs = st.executeQuery(); // guarda o resultado dentro de um result set
 
 			List<Seller> list = new ArrayList<>();
-			Map<Integer, Department> map = new HashMap<>();//map(key,value)
-			
+			Map<Integer, Department> map = new HashMap<>();// map(key,value)
+
 			while (rs.next()) {
-				Department dep = map.get(rs.getInt("DepartmentId"));// verifica dentro do map se o departamento já existe se sim não cria 
+				Department dep = map.get(rs.getInt("DepartmentId"));// verifica dentro do map se o departamento já
+																	// existe se sim não cria
 				if (dep == null) {
-					dep = instantiateDepartment(rs);//instancia novo departamento
-					map.put(rs.getInt("DepartmentId"), dep);//coloca no map
+					dep = instantiateDepartment(rs);// instancia novo departamento
+					map.put(rs.getInt("DepartmentId"), dep);// coloca no map
 				}
 				Seller obj = instantiateSeller(rs, dep);
-				list.add(obj);		
+				list.add(obj);
 			}
 			return list;
 		} catch (SQLException e) {
@@ -128,39 +159,36 @@ public class SellerDaoJDBC implements SellerDao{
 		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		}		
-		
-		
+		}
+
 	}
 
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		
+
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
 			st.setInt(1, department.getId());// faz a consulta passando o id
 			rs = st.executeQuery(); // guarda o resultado dentro de um result set
 
 			List<Seller> list = new ArrayList<>();
-			Map<Integer, Department> map = new HashMap<>();//map(key,value)
-			
+			Map<Integer, Department> map = new HashMap<>();// map(key,value)
+
 			while (rs.next()) {
-				Department dep = map.get(rs.getInt("DepartmentId"));// verifica dentro do map se o departamento já existe se sim não cria 
+				Department dep = map.get(rs.getInt("DepartmentId"));// verifica dentro do map se o departamento já
+																	// existe se sim não cria
 				if (dep == null) {
-					dep = instantiateDepartment(rs);//instancia novo departamento
-					map.put(rs.getInt("DepartmentId"), dep);//coloca no map
+					dep = instantiateDepartment(rs);// instancia novo departamento
+					map.put(rs.getInt("DepartmentId"), dep);// coloca no map
 				}
 				Seller obj = instantiateSeller(rs, dep);
-				list.add(obj);		
+				list.add(obj);
 			}
 			return list;
 		} catch (SQLException e) {
@@ -168,7 +196,7 @@ public class SellerDaoJDBC implements SellerDao{
 		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		}		
+		}
 	}
 
 }
